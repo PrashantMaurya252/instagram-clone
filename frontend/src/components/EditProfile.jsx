c20db6673053cf1ee04ac1484f3a818911c439b2
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-11-20 18:24:42 +0530

    EditProfile Page making

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -1,17 +1,57 @@
-import { Link } from "react-router-dom";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "./ui/button";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Textarea } from "./ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import axios from "axios";
+import { Loader2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const EditProfile = () => {
   const { user } = useSelector((store) => store.auth);
-  const imageRef = useRef()
-  
+  const imageRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState({
+    profilePhoto: user?.profilePicture,
+    bio: user?.bio,
+    gender: user?.gender,
+  });
+
+  const selectChangeHandler = (value)=>{
+   setInput({...input,gender:value})
+  }
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const fileChangeHandler = (e) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setInput({ ...input, profilePhoto: file });
+    }
+  };
+
+  const editProfileHandler = async () => {
+    console.log(input)
+    // try {
+    //   setLoading(true);
+    //   const res = axios.post();
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+
   return (
     <div className="flex max-w-2xl mx-auto pl-10">
-      <section className="flex flex-col gap-6 w-full">
+      <section className="flex flex-col gap-6 w-full my-8">
         <h1 className="font-bold text-xl">Edit Profile</h1>
         <div className="flex items-center justify-between bg-gray-100 rounded-xl p-4">
           <div className="flex items-center gap-3">
@@ -27,12 +67,43 @@ const EditProfile = () => {
               </span>
             </div>
           </div>
-          <input ref={imageRef} type="file" className="hidden"/>
-          <Button onClick={()=>imageRef?.current.click()} className="bg-[#0095F6] h-8 rounded-lg hover:bg-[#318bc7]">Change photo</Button>
+          <input ref={imageRef} type="file" className="hidden" />
+          <Button
+            onClick={() => imageRef?.current.click()}
+            className="bg-[#0095F6] h-8 rounded-lg hover:bg-[#318bc7]"
+          >
+            Change photo
+          </Button>
         </div>
         <div>
-            <h1>Bio</h1>
-            <Textarea/>
+          <h1 className="font-bold text-xl mb-2">Bio</h1>
+          <Textarea value={input?.bio} onChange={(e)=>setInput({...input,bio:e.target.value})} name="bio" className="focus-visible:ring-transparent" />
+        </div>
+        <div>
+          <h1 className="font-bold mb-2">Gender</h1>
+          <Select defaultValue={input?.gender} onValueChange={selectChangeHandler}>
+            <SelectTrigger className="w-full">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="male">Male</SelectItem>
+                <SelectItem value="female">Female</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex justify-end">
+          {loading ? (
+            <Button className="w-fit bg-[#0095F6] hover:bg-[#2a8ccd]">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait
+            </Button>
+          ) : (
+            <Button onClick={editProfileHandler} className="w-fit bg-[#0095F6] hover:bg-[#2a8ccd]">
+              Submit
+            </Button>
+          )}
         </div>
       </section>
     </div>
